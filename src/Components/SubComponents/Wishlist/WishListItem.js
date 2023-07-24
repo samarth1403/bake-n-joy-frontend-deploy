0a5cda94e62d5bd4ea4f2577a9fddfac112059f9
Samarth Parmeshwar Ikkalaki <samarthikkalaki123@gmail.com>
2023-07-24 20:23:23 +0530

    Mistake 1.7 Updated

--- src/Components/SubComponents/Wishlist/WishListItem.js
+++ src/Components/SubComponents/Wishlist/WishListItem.js
@@ -24,59 +24,70 @@ const WishListItem = ({ wishlistItem }) => {
     }, 100);
   }
   return (
-    <div
-      className="relative flex flex-col flex-no-wrap flex-shrink-0 justify-start items-center w-[260px] h-auto rounded-[33px] mb-16 mt-4"
-      style={{
-        background: "linear-gradient(180deg, #FAFAFA 46.2%, #101567 100%)",
-      }}
-    >
-      <button
-        onClick={() => removeFromWishlist(wishlistItem._id)}
-        className="btn-close absolute top-4 right-4"
+    <div className="p-4">
+      <div
+        className="relative flex flex-col flex-no-wrap flex-shrink-0 justify-start items-center w-[260px] h-auto rounded-[33px] mb-16 mt-4"
+        style={{
+          background: "linear-gradient(180deg, #FAFAFA 46.2%, #101567 100%)",
+        }}
       >
-        <AiFillCloseCircle className="text-4xl text-red-500" />
-      </button>
-      <Link to={`/product-details/${wishlistItem._id}`} onClick={()=>ScrollToTop()}>
-        <img
-          src={wishlistItem?.images[0]?.url}
-          alt="Shopping Item"
-          className="w-[260px] h-[260px] rounded-t-[33px] mb-2"
-        />
-      </Link>
-
-      <div className="flex flex-col flex-no-wrap">
-        <p className="font-roboto font-bold text-[#090b2b] text-lg px-2 mt-3 m-1">
-          {wishlistItem.title}
-        </p>
-        <p className="font-roboto font-bold text-[#090b2b] text-lg px-2 m-1">
-          Rs.{wishlistItem.price}/-
-        </p>
-        <div className="mx-2">
-          <ReactStars
-            count={5}
-            // onChange={ratingChanged}
-            value={wishlistItem.totalRating}
-            size={24}
-            isHalf={true}
-            edit={false}
-            a11y={true}
-            emptyIcon={<i className="far fa-star"></i>}
-            halfIcon={<i className="fa fa-star-half-alt"></i>}
-            fullIcon={<i className="fa fa-star"></i>}
-            activeColor="#ECD400"
+        <button
+          onClick={() => removeFromWishlist(wishlistItem._id)}
+          className="btn-close absolute top-4 right-4"
+        >
+          <AiFillCloseCircle className="text-4xl text-red-500" />
+        </button>
+        <Link
+          to={`/product-details/${wishlistItem._id}`}
+          onClick={() => ScrollToTop()}
+        >
+          <img
+            src={wishlistItem?.images[0]?.url}
+            alt="Shopping Item"
+            className="w-[260px] h-[260px] rounded-t-[33px] mb-2"
           />
-        </div>
-        <div className="flex flex-row flex-no-wrap justify-between px-2 pt-3 pb-6">
-          <Link to={`/product-details/${wishlistItem?._id}`} onClick={()=>ScrollToTop()}>
-            <ButtonRYG className="px-4 mr-4 py-2 rounded-[12px]">
-              Add to Cart
-            </ButtonRYG>
-          </Link>
-          <Link to={`/product-details/${wishlistItem?._id}`} onClick={()=>ScrollToTop()}>
-            <ButtonRYG className="px-4 mr-4 py-2 rounded-[12px]">
-              Buy Now
-            </ButtonRYG>
-          </Link>
+        </Link>
+
+        <div className="flex flex-col flex-no-wrap">
+          <p className="font-roboto font-bold text-[#090b2b] text-lg px-2 mt-3 m-1">
+            {wishlistItem.title}
+          </p>
+          <p className="font-roboto font-bold text-[#090b2b] text-lg px-2 m-1">
+            Rs.{wishlistItem.price}/-
+          </p>
+          <div className="mx-2">
+            <ReactStars
+              count={5}
+              // onChange={ratingChanged}
+              value={wishlistItem.totalRating}
+              size={24}
+              isHalf={true}
+              edit={false}
+              a11y={true}
+              emptyIcon={<i className="far fa-star"></i>}
+              halfIcon={<i className="fa fa-star-half-alt"></i>}
+              fullIcon={<i className="fa fa-star"></i>}
+              activeColor="#ECD400"
+            />
+          </div>
+          <div className="flex flex-row flex-no-wrap justify-between px-2 pt-3 pb-6">
+            <Link
+              to={`/product-details/${wishlistItem?._id}`}
+              onClick={() => ScrollToTop()}
+            >
+              <ButtonRYG className="px-4 mr-4 py-2 rounded-[12px]">
+                Add to Cart
+              </ButtonRYG>
+            </Link>
+            <Link
+              to={`/product-details/${wishlistItem?._id}`}
+              onClick={() => ScrollToTop()}
+            >
+              <ButtonRYG className="px-4 mr-4 py-2 rounded-[12px]">
+                Buy Now
+              </ButtonRYG>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
